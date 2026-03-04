88417f0d0c67500302b60b65396e9ca7c1c76adc
Parth <parth.ramanujj@gmail.com>
2026-03-04 20:40:39 +0530

    Add fallback STUN servers to fix WebRTC timeouts

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,13 @@ webrtc_streamer(
         "audio": False
     },
     rtc_configuration={
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+        "iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]},
+            {"urls": ["stun:stun1.l.google.com:19302"]},
+            {"urls": ["stun:stun2.l.google.com:19302"]},
+            {"urls": ["stun:stun.stunprotocol.org:3478"]},
+            {"urls": ["stun:stun.twilio.com:3478"]}
+        ]
     },
     async_processing=True
 )
